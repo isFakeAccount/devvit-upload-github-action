ef9f84ef04ed5c4c51e1eaf424eda08da6c620c5
Yoshikage Kira <trevorphillips@gmx.us>
2023-07-22 18:10:00 -0400

    Setting node, devvit, and py
    Testing if py, node, and devvit works

---
+++ README.md
@@ -0,0 +1,7 @@
+# Devvit App Upload GitHub Action
+
+The Devvit App Upload GitHub Action is a custom GitHub Action designed to streamline the process of uploading apps to the [Devvit](https://developers.reddit.com/) platform. 
+
+## Usage
+
+## Inputs
---
+++ token_file.py
@@ -0,0 +1,12 @@
+import json
+import sys
+from pathlib import Path
+
+if __name__ == "__main__":
+    refresh_token = sys.argv[2]
+    devvit_token = {"token": refresh_token, "copyPaste": False}
+
+    devvit_dir = Path.home() / ".devvit"
+    devvit_dir.mkdir(exist_ok=True)
+    token_file = Path(devvit_dir.absolute(), "token")
+    token_file.write_text(json.dumps(devvit_token))
